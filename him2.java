7ed26c803e02b0fd40dce48bd463a32adc402787
myhim <44494991+myhim@users.noreply.github.com>
2018-10-26 05:35:20 -0400

    Create him2.java

---
+++ him2.java
@@ -0,0 +1,17 @@
+public class check{
+public static void main(String args[]){
+int n=3;
+if(n<0)
+{
+System.out.println(n + "number is not valid");
+}
+else if(n%2==0)
+{
+System.out.println(n + "number is even");
+}
+else{
+System.out.println(n + "number is odd");
+}
+}
+}
+
