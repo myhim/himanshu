f49860373017c5f91885422a9a3b27b9af57adce
myhim <44494991+myhim@users.noreply.github.com>
2018-10-26 05:22:05 -0400

    Create him1.java

---
+++ him1.java
@@ -0,0 +1,12 @@
+public class number{
+public static void main(String args[]){
+int n=3;
+if(n>0)
+System.out.println(n + " is positive");
+
+else if(n<0)
+System.out.println(n + " is negative");
+else
+System.out.println("zero");
+}
+}
